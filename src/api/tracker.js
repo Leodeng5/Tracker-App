b08d9c70d65fced86e131a94fc928569403f919d
leodeng5 <leodeng@usc.edu>
2022-07-13 17:08:40 -0700

    Tracker App finished

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default axios.create({
+const instance = axios.create({
   baseURL: "https://0207-2603-8000-ba00-3674-4877-79c5-d4f6-1029.ngrok.io",
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
